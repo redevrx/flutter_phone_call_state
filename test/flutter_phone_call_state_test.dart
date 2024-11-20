67ea5d93022106d05e3e1f4eb6938527246018dc
redev.rx <redev.rx@gmail.com>
2024-11-20 23:01:41 +0700

    add test

--- test/flutter_phone_call_state_test.dart
+++ test/flutter_phone_call_state_test.dart
@@ -15,6 +15,7 @@ class MockFlutterPhoneCallStatePlatform
   }
 }
 
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   final FlutterPhoneCallStatePlatform initialPlatform =
@@ -24,18 +25,126 @@ void main() {
     expect(initialPlatform, isInstanceOf<MethodChannelFlutterPhoneCallState>());
   });
 
-  test('test phone state change', () async {
-    final flutterPhoneCallStatePlugin = PhoneCallState.instance;
-    MockFlutterPhoneCallStatePlatform fakePlatform =
-        MockFlutterPhoneCallStatePlatform();
-    FlutterPhoneCallStatePlatform.instance = fakePlatform;
-    final result = CallResult(state: CallState.call, number: "0857200286");
+  group('CallState Enum Tests', () {
+    test('should return correct description for each CallState', () {
+      expect(CallState.end.description, 'End call');
+      expect(CallState.outgoing.description, 'Dialing outgoing call');
+      expect(CallState.outgoingAccept.description, 'Outgoing call accepted');
+      expect(CallState.incoming.description, 'Incoming call');
+      expect(CallState.call.description, 'Call accepted');
+      expect(CallState.none.description, 'Unknown call state');
+    });
 
-    flutterPhoneCallStatePlugin.phoneStateChange.listen(
-      (event) {
-        expect(event.number, result.number);
-        expect(event.state, result.state);
-      },
-    );
+    test('should return correct values for all CallState enums', () {
+      expect(CallState.end, CallState.end);
+      expect(CallState.outgoing, CallState.outgoing);
+      expect(CallState.outgoingAccept, CallState.outgoingAccept);
+      expect(CallState.incoming, CallState.incoming);
+      expect(CallState.call, CallState.call);
+      expect(CallState.none, CallState.none);
+    });
+
+    test('should have 6 states', () {
+      expect(CallState.values.length, 6);
+    });
+
+    test('should return the correct index for each CallState', () {
+      expect(CallState.end.index, 0);
+      expect(CallState.outgoing.index, 1);
+      expect(CallState.outgoingAccept.index, 2);
+      expect(CallState.incoming.index, 3);
+      expect(CallState.call.index, 4);
+      expect(CallState.none.index, 5);
+    });
+
+    test('should check if CallState is "outgoing" correctly', () {
+      expect(CallState.outgoing == CallState.outgoing, isTrue);
+      expect(CallState.outgoing == CallState.incoming, isFalse);
+    });
   });
+
+  group('test call result ', () {
+    test('should create CallResult from status and phone number', () {
+      final data = {
+        'status': 2,
+        'phoneNumber': '+1234567890',
+      };
+
+      final result = CallResult.fromFlag(data);
+
+      expect(result.state, CallState.incoming);
+      expect(result.number, '+1234567890');
+    });
+
+    test('should return default CallState.none when invalid status is provided', () {
+      final data = {
+        'status': -1,
+        'phoneNumber': '+0987654321',
+      };
+
+      final result = CallResult.fromFlag(data);
+
+      expect(result.state, CallState.none);
+      expect(result.number, '+0987654321');
+    });
+
+    test('should return empty string if no phone number is provided', () {
+      final data = {
+        'status': 3,
+      };
+
+      final result = CallResult.fromFlag(data);
+
+      expect(result.state, CallState.call);
+      expect(result.number, '');
+    });
+
+    test('test call state enum and Result State', () {
+      final result = CallResult(state: CallState.call,number: '01');
+      expect(result, isA<CallResult>());
+      expect(result.state, CallState.call);
+      expect(result.number.isNotEmpty, true);
+    },);
+
+    test('test call result from', () {
+      final result = CallResult.from(0);
+      expect(result, isA<CallState>());
+      expect(result, CallState.end);
+    },);
+
+    test('test call result from flag', () {
+      final result = CallResult.fromFlag({"status":0,'phoneNumber':'01'});
+      expect(result, isA<CallResult>());
+      expect(result.state, CallState.end);
+      expect(result.number.isNotEmpty, true);
+    },);
+  },);
+
+  group('test platform interface', () {
+    test('test phone state change', () async {
+      final flutterPhoneCallStatePlugin = PhoneCallState.instance;
+      MockFlutterPhoneCallStatePlatform fakePlatform =
+      MockFlutterPhoneCallStatePlatform();
+      FlutterPhoneCallStatePlatform.instance = fakePlatform;
+      final result = CallResult(state: CallState.call, number: "0857200286");
+
+      flutterPhoneCallStatePlugin.phoneStateChange.listen(
+            (event) {
+          expect(event.number, result.number);
+          expect(event.state, result.state);
+        },
+      );
+    });
+
+    test('should allow setting a valid instance', () {
+      // Arrange: Create a mock instance of FlutterPhoneCallStatePlatform
+      final mockPlatform = MockFlutterPhoneCallStatePlatform();
+
+      // Act: Set the instance using the setter
+      FlutterPhoneCallStatePlatform.instance = mockPlatform;
+
+      // Assert: Verify that the setter has correctly set the instance
+      expect(FlutterPhoneCallStatePlatform.instance, mockPlatform);
+    });
+  },);
 }
