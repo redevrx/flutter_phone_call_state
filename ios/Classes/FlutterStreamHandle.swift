535d3b00bb7ce1d56a30e7bcbb3109db893f4e9e
redev.rx <redev.rx@gmail.com>
2025-01-08 15:35:02 +0700

    add ios phone number

--- ios/Classes/FlutterStreamHandle.swift
+++ ios/Classes/FlutterStreamHandle.swift
@@ -26,26 +26,28 @@ class FlutterStreamHandle : NSObject, FlutterStreamHandler,CXCallObserverDelegat
 
    @available(iOS 10.0, *)
      public func callObserver(_ callObserver: CXCallObserver, callChanged call: CXCall) {
+     let phoneNumber = call.handle?.value(forKey: "digits") as? String ?? ""
+
           if call.hasEnded {
                     // Phone call ended
-           send(data:["status": 0, "phoneNumber": ""])
+           send(data:["status": 0, "phoneNumber": phoneNumber])
           } else if call.isOutgoing && !call.hasConnected {
                      // Outgoing call, dialing
-            send(data:["status": 1, "phoneNumber": ""]) // 1 for dialing
+            send(data:["status": 1, "phoneNumber": phoneNumber]) // 1 for dialing
             isOutgoing = true
           } else if !call.isOutgoing && !call.hasConnected && !call.hasEnded {
                      // Incoming call, waiting to connect
-             send(data:["status": 2, "phoneNumber": ""]) // 2 for incoming call
+             send(data:["status": 2, "phoneNumber": phoneNumber]) // 2 for incoming call
           } else if call.hasConnected && !call.hasEnded {
                      // Call is connected
               if(isOutgoing){
-               send(data:["status": 4, "phoneNumber": ""]) // 4 for Outgoing connected call
+               send(data:["status": 4, "phoneNumber": phoneNumber]) // 4 for Outgoing connected call
                isOutgoing = false
               }else {
-               send(data:["status": 3, "phoneNumber": ""]) // 3 for connected call
+               send(data:["status": 3, "phoneNumber": phoneNumber]) // 3 for connected call
               }
           } else {
-             send(data:["status": -1, "phoneNumber": ""])
+             send(data:["status": -1, "phoneNumber": phoneNumber])
           }
      }
 
