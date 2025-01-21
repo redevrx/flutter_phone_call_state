989995ae71f14527cf3d68d6ff4b663e7539cfcd
redev.rx <redev.rx@gmail.com>
2025-01-21 11:24:41 +0700

    add callback ios

--- ios/Classes/FlutterStreamHandle.swift
+++ ios/Classes/FlutterStreamHandle.swift
@@ -74,3 +74,58 @@ class FlutterStreamHandle : NSObject, FlutterStreamHandler,CXCallObserverDelegat
             self.callback = callback
     }
 }
+
+
+class CallStateHandle : NSObject,CXCallObserverDelegate {
+    private var callObserver: CXCallObserver!
+    private var isOutgoing = false
+    private var callback: (([String: Any]) -> Void)?
+
+    override init() {
+       super.init()
+       setupCallObserver()
+    }
+
+   @available(iOS 10.0, *)
+   private func setupCallObserver() {
+        callObserver = CXCallObserver()
+        callObserver.setDelegate(self, queue: nil)
+   }
+
+   @available(iOS 10.0, *)
+     public func callObserver(_ callObserver: CXCallObserver, callChanged call: CXCall) {
+     let phoneNumber = ""
+
+          if call.hasEnded {
+                    // Phone call ended
+           send(data:["status": 0, "phoneNumber": phoneNumber])
+          } else if call.isOutgoing && !call.hasConnected {
+                     // Outgoing call, dialing
+            send(data:["status": 1, "phoneNumber": phoneNumber]) // 1 for dialing
+            isOutgoing = true
+          } else if !call.isOutgoing && !call.hasConnected && !call.hasEnded {
+                     // Incoming call, waiting to connect
+             send(data:["status": 2, "phoneNumber": phoneNumber]) // 2 for incoming call
+          } else if call.hasConnected && !call.hasEnded {
+                     // Call is connected
+              if(isOutgoing){
+               send(data:["status": 4, "phoneNumber": phoneNumber]) // 4 for Outgoing connected call
+               isOutgoing = false
+              }else {
+               send(data:["status": 3, "phoneNumber": phoneNumber]) // 3 for connected call
+              }
+          } else {
+             send(data:["status": -1, "phoneNumber": phoneNumber])
+          }
+     }
+
+    public func send(data:[String: Any]){
+    if let callback = callback {
+                callback(data)
+            }
+    }
+
+    public func setCallback(callback: @escaping ([String: Any]) -> Void) {
+            self.callback = callback
+    }
+}
