1dabc0b3a41930df3b3e4a7b4adc4c976b48848d
redev.rx <redev.rx@gmail.com>
2025-01-24 17:50:49 +0700

    feat: ios auto request background task

--- ios/Classes/FlutterPhoneCallStatePlugin.swift
+++ ios/Classes/FlutterPhoneCallStatePlugin.swift
@@ -2,21 +2,39 @@ import Flutter
 import UIKit
 
 public class FlutterPhoneCallStatePlugin: NSObject, FlutterPlugin {
-   var handler:FlutterStreamHandle?
+//    var handler:FlutterStreamHandle?
+//
+//    public static func register(with registrar: FlutterPluginRegistrar) {
+//         let instance = FlutterPhoneCallStatePlugin()
+//          self.handler = FlutterStreamHandle()
+//
+//         let eventChannel = FlutterEventChannel(name: "flutter_phone_call_state", binaryMessenger: registrar.messenger())
+//         eventChannel.setStreamHandler(self.handler!)
+//
+//         ///handler.beginBackgroundMonitoring()
+//      }
+//
+//      public func initState(){
+//      self.handler?.reSetup()
+//      }
+
+   var handler: FlutterStreamHandle?
+
+       // Shared instance
+   public static let shared = FlutterPhoneCallStatePlugin()
 
    public static func register(with registrar: FlutterPluginRegistrar) {
-        let instance = FlutterPhoneCallStatePlugin()
-         self.handler = FlutterStreamHandle()
+           let instance = FlutterPhoneCallStatePlugin.shared // ใช้ shared instance
+           instance.handler = FlutterStreamHandle()
 
-        let eventChannel = FlutterEventChannel(name: "flutter_phone_call_state", binaryMessenger: registrar.messenger())
-        eventChannel.setStreamHandler(self.handler!)
+           let eventChannel = FlutterEventChannel(name: "flutter_phone_call_state", binaryMessenger: registrar.messenger())
+           eventChannel.setStreamHandler(instance.handler!)
+   }
 
-        ///handler.beginBackgroundMonitoring()
-     }
+   public func initState() {
+           self.handler?.reSetup()
+   }
 
-     public func initState(){
-     self.handler?.reSetup()
-     }
 }
 
 // public class FlutterPhoneCallStatePlugin: NSObject, FlutterPlugin, CXCallObserverDelegate, FlutterStreamHandler {
